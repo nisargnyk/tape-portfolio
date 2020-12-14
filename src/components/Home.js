2cff6ecd6fff28e18d17501b33c45b7a7c914ea9
Nisarg Nyk <nisarg.nayak@outlook.com>
2020-12-14 12:01:50 +0530

    entire portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import image from "../image.jpg"
+
+export default function Home(){
+    return (
+        <main>
+            <img src={image} alt="image" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-blod cursive leading-none lg:leading-snug home-name">Aloha. I'm Nisarg Nyk.</h1>
+            </section>
+        </main>
+    )
+}
